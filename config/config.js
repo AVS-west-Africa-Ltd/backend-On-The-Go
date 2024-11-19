b542eb99d39f9787baaa40141f46a278054f934e
otitoju1 <john.o@allprimetech.com>
2024-11-19 11:45:57 +0100

    chores: setup sequelize

---
+++ config/config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+development: {
+username: process.env.DB_USERNAME || 'root',
+password: process.env.DB_PASSWORD || '',
+database: process.env.DB_DATABASE_DEV || 'default_dev_db',
+host: process.env.DB_HOST || 'localhost',
+port: process.env.DB_PORT || 3306,
+dialect: 'mysql',
+},
+production: {
+username: process.env.DB_USERNAME_PROD || 'root',
+password: process.env.DB_PASSWORD_PROD || '',
+database: process.env.DB_DATABASE_PROD || 'default_prod_db',
+host: process.env.DB_HOST_PROD || 'localhost',
+port: process.env.DB_PORT_PROD || 3306,
+dialect: 'mysql',
+},
+};
--- models/User.js
+++ models/User.js
@@ -1,21 +1,41 @@
-const pool = require('../config/db');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
-const User = {
-  create: async (username, email, password) => {
-    const [rows] = await pool.query(
-      'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
-      [username, email, password]
-    );
-    return rows.insertId;
-  },
-  findByUsername: async (username) => {
-    const [rows] = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
-    return rows[0];
-  },
-  findByEmail: async (email) => {
-    const [rows] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
-    return rows[0];
-  },
-};
+const userSchema = sequelize.define('Users', {
+    firstName: {
+        type: DataTypes.STRING,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+    },
+    username: {
+        type: DataTypes.STRING,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone_number: {
+        type: DataTypes.STRING,
+    },
+    picture: {
+        type: DataTypes.TEXT,
+    },
+    bio: {
+        type: DataTypes.TEXT,
+    },
+    interests: {
+        type: DataTypes.JSON,
+    },
+    notifications: {
+        type: DataTypes.JSON,
+    }
+});
 
-module.exports = User;
+sequelize.sync().then(() => {
+    console.log('User Schema table created successfully!');
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
+
+module.exports = userSchema;

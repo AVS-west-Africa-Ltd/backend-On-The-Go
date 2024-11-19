75ad80e4779de946bad76aaec6ff322471907719
otitoju1 <john.o@allprimetech.com>
2024-11-19 11:45:57 +0100

    chores: setup sequelize

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+require('dotenv').config();
 const express = require('express');
-require('./config/dotenv'); // Load environment variables
-const authRoutes = require('./routes/authRoutes');
-const profileRoutes = require('./routes/profileRoutes');
-const postRoutes = require('./routes/postRoutes');
-const locationRoutes = require('./routes/locationRoutes');
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
-app.use(express.json());
+require("./config/database");
+const router = require("./routes/routes");
+const PORT = process.env.PORT || 5000;
 
-app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
-app.use('/posts', postRoutes); 
-app.use('/locations', locationRoutes); 
 
-const PORT = process.env.PORT || 3000;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/v1/", router);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });

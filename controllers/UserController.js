b8157819b2fe01fb59d55923e98ee80440970acd
Abdulrazaq Oladimeji <abdulrazaq2a@gmail.com>
2025-01-06 13:04:32 +0100

    save

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -34,7 +34,7 @@ class UserController {
       const { username, email, password } = req.body;
 
       if (!username || !email || !password)
-        return res.status(400).json({ message: "All fields are required" });
+        return res.status(400).json({ message: "All mfields are required" });
 
       let payload = { where: { email: email } };
       const isUserRegistered = await userService.getUserByEmailOrUsername(

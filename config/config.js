73d9e5344566ffd96bdd18e5dece53ae1b043f1c
otitoju1 <otitojuoluwapelumi@gmail.com>
2025-02-06 12:15:19 +0100

    audit packages

--- config/config.js
+++ config/config.js
@@ -18,13 +18,21 @@ module.exports = {
             dialect: 'mysql',
         },
         production: {
-            username: "onthelbo_afrocode2k",
-            password: "Justkeepitsimple2@",
-            database:"onthelbo_otgdb",
+            username: "onthego",
+            password: "jsmre8JPhtKmnw72",
+            database:"onthego",
             host:"localhost",
             port: 3306,
             dialect: 'mysql',
         },
+        // production: {
+        //     username: "onthelbo_afrocode2k",
+        //     password: "Justkeepitsimple2@",
+        //     database:"onthelbo_otgdb",
+        //     host:"localhost",
+        //     port: 3306,
+        //     dialect: 'mysql',
+        // },
     APIKEY: '26a3281bfc65b39527447691941d6a707357a1278b1b2ec91742faec9de53ac8'
 };
 

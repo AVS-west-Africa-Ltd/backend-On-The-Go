cfaab77c30e73edba1e0a8be93516d0d1beb5464
otitoju <otitojuoluwapelumi@gmail.com>
2025-02-09 20:32:09 +0100

    add otg database

--- config/database.js
+++ config/database.js
@@ -13,12 +13,12 @@ const config = require("./config");
 // });
 
 const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
+  config.production.database,
+  config.production.username,
+  config.production.password,
   {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
+    host: config.production.host,
+    port: config.production.port,
     dialect: "mysql",
   }
 );

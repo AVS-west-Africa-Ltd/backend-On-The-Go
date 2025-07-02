efcf9275d3c236d8bb5606146e177971dda82e29
Idris Abdulkadri <38693336+kadrikondi@users.noreply.github.com>
2025-07-02 13:58:45 +0100

    Update app.js
    PORT NUMBER

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0';
 const app = express();
 

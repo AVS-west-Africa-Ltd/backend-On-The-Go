9d1b3c31231ec566dbd27fd1d99958878a0406a0
markiechan <70292382+markwritecode@users.noreply.github.com>
2025-07-21 14:21:52 +0100

    Database config file clean up

--- config/database.js
+++ config/database.js
@@ -3,50 +3,17 @@ const { Sequelize } = require("sequelize");
 const path = require("path");
 const log = require('../utils/logger'); 
 
-const env = process.env.NODE_ENV || "production";
-// const config = require(path.join(__dirname, '../config/config.js'))[env];
-const config = require("./config");
-
-// const sequelize = new Sequelize(config.database, config.username, config.password, {
-//   host: process.env.DB_HOST || 'localhost',
-//   port: process.env.DB_PORT || 3306,
-//   dialect: 'mysql',
-// });
-//
-// const sequelize = new Sequelize(
-//   config.production.database,
-//   config.production.username,
-//   config.production.password,
-//   {
-//     host: config.production.host,
-//     port: config.production.port,
-//     dialect: "mysql",
-//     logging: (msg) => log(`Sequelize: ${msg}`),
-//   }
-// );
 
 const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME || 'root',
+  process.env.DB_PASSWORD,
   {
-    host: config.development.host,
-    port: config.development.port,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
   }
 );
-
-// const sequelize = new Sequelize(
-//   config.production.database,
-//   config.production.username,
-//   config.production.password,
-//   {
-//     host: config.production.host,
-//     port: config.production.port,
-//     dialect: "mysql",
-//     logging: (msg) => log(`Sequelize: ${msg}`),
-//   }
-// );
 
 sequelize
   .authenticate()

b8aab51cb81644c19e81513978ea9fbd1167a63d
Khaybee <kachieosuji11@gmail.com>
2025-12-20 20:27:20 +0100

    fix: cors config

--- src/app.ts
+++ src/app.ts
@@ -32,19 +32,11 @@ const HOST = '0.0.0.0';
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
-
-// CORS Headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(cors({
+  origin: "*", // tighten later
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 
 // app.use(validateApiKey);
